3e071f0819d59add42ca28d28f9a7655819782f0
Justin Lee <triplez@justinlee.sg>
2024-05-31 16:39:58 +0800

    chore: Update monitoring.sh and preseed.cfg scripts

--- monitoring.sh
+++ monitoring.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
-ARCH=$(uname -srvmo)
+ARCH=$(uname --all)
 PCPU=$(grep 'physical id' /proc/cpuinfo | uniq | wc -l)
 VCPU=$(grep processor /proc/cpuinfo | uniq | wc -l)
-RAM_TOTAL=$(free -h | grep Mem | awk '{print $2}')
-RAM_USED=$(free -h | grep Mem | awk '{print $3}')
-RAM_PERC=$(free -k | grep Mem | awk '{printf("%.2f%%"), $3 / $2 * 100}')
-DISK_TOTAL=$(df -h --total | grep total | awk '{print $2}')
-DISK_USED=$(df -h --total | grep total | awk '{print $3}')
-DISK_PERC=$(df -k --total | grep total | awk '{print $5}')
-CPU_LOAD=$(top -bn1 | grep '^%Cpu' | xargs | awk '{printf("%.1f%%"), $2 + $4}')
-LAST_BOOT=$(who -b | awk '{print($3 " " $4)}')
-LVM=$(if [ "$(lsblk | grep -c lvm)" -eq 0 ]; then echo no; else echo yes; fi)
+RAM_TOTAL=$(free --mega --human | grep 'Mem' | awk '{print $2}')
+RAM_USED=$(free --mega | grep 'Mem' | awk '{print $3}')
+RAM_PERC=$(free -mega | grep 'Mem' | awk '{printf("%.2f%%"), $3 / $2 * 100}')
+DISK_TOTAL=$(df --human-readable --total | grep 'total' | awk '{print $2}')
+DISK_USED=$(df --human-readable -B1M --total | grep 'total' | awk '{print $3}')
+DISK_PERC=$(df --human-readable --total | grep 'total' | awk '{print $5}')
+CPU_LOAD=$(top --batch-mode --iterations 1 | grep '^%Cpu' | xargs | awk '{printf("%.1f%%"), $2 + $4}')
+LAST_BOOT=$(who --boot | awk '{print($3 " " $4)}')
+LVM=$(if [ "$(lsblk | grep --count lvm)" -eq 0 ]; then echo no; else echo yes; fi)
 TCP=$(grep TCP /proc/net/sockstat | awk '{print $3}')
 USER_LOG=$(who | wc -l)
 IP_ADDR=$(hostname -I | awk '{print $1}')
-MAC_ADDR=$(ip link show | grep link/ether | awk '{print $2}')
-SUDO_LOG=$(grep -c COMMAND /var/log/sudo/sudo.log)
+MAC_ADDR=$(ip address | grep link/ether | awk '{print $2}')
+SUDO='/usr/bin/sudo'
+SUDO_LOG=$([[ -d ${SUDO} ]] || journalctl -e "${SUDO}" | grep --count COMMAMND)
 
 wall "
-       ------------------------------------------------
-       Architecture    : $ARCH
-       Physical CPUs   : $PCPU
-       Virtual CPUs    : $VCPU
-       Memory Usage    : $RAM_USED/$RAM_TOTAL ($RAM_PERC)
-       Disk Usage      : $DISK_USED/$DISK_TOTAL ($DISK_PERC)
-       CPU Load        : $CPU_LOAD
-       Last Boot       : $LAST_BOOT
-       LVM use         : $LVM
-       TCP Connections : $TCP established
-       Users logged    : $USER_LOG
-       Network         : $IP_ADDR ($MAC_ADDR)
-       Sudo            : $SUDO_LOG commands used
-       ------------------------------------------------"
+    #Architecture: $ARCH
+    #CPU physical: $PCPU
+    #vCPU: $VCPU
+    #Memory Usage: $RAM_USED/$RAM_TOTAL ($RAM_PERC)
+    #Disk Usage: $DISK_USED/$DISK_TOTAL ($DISK_PERC)
+    #CPU Load: $CPU_LOAD
+    #Last Boot: $LAST_BOOT
+    #LVM use: $LVM
+    #Connections TCP: $TCP established
+    #Users log: $USER_LOG
+    #Network: IP $IP_ADDR ($MAC_ADDR)
+    #Sudo: $SUDO_LOG cmd"
