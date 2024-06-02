fbe49f8b783e01f8cf34aad2da4c53ef96afef21
Justin Lee <triplez@justinlee.sg>
2024-06-02 11:09:43 +0800

    chore: Update wget URLs in cron.sh and preseed.cfg for mandatory scripts

--- mandatory/cron.sh
+++ mandatory/cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O /usr/local/bin/monitoring.sh http://c2r1s14.42singapore.sg:8000/monitoring.sh
+wget -O /usr/local/bin/monitoring.sh http://c2r1s14.42singapore.sg:8000/mandatory/monitoring.sh
 chmod +x /usr/local/bin/monitoring.sh
 echo '*/10 * * * * /usr/local/bin/monitoring.sh' | crontab -u welee -
