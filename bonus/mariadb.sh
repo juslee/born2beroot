131f73f1784d395c1f037ae5f0f1c447d55f7211
Justin Lee <triplez@justinlee.sg>
2024-06-02 21:45:34 +0800

    Refactor MariaDB and WordPress setup scripts

--- bonus/mariadb.sh
+++ bonus/mariadb.sh
@@ -2,20 +2,36 @@
 systemctl enable mariadb
 systemctl start mariadb
 
+apt install -y expect
+
 MYSQL_ROOT_PASSWORD="mypassword"
 MYSQL_USER_PASSWORD="mypassword"
 
-mysql -u root -e "
-UPDATE mysql.user SET plugin = 'unix_socket' WHERE User = 'root';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-DELETE FROM mysql.user WHERE User = '';
-UPDATE mysql.user SET Host='localhost' WHERE User='root' AND Host='%';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEGES;
-"
-systemctl restart mariadb
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "
+expect -f - <<-EOF
+set timeout 2
+spawn mysql_secure_installation
+expect "Enter current password for root (enter for none):"
+send "\r"
+expect "Switch to unix_socket authentication \[Y/n\]"
+send "n\r"
+expect "Change the root password? \[Y/n\]"
+send "Y\r"
+expect "New password:"
+send "${MYSQL_ROOT_PASSWORD}\r"
+expect "Re-enter new password:"
+send "${MYSQL_ROOT_PASSWORD}\r"
+expect "Remove anonymous users?  \[Y/n\]"
+send "Y\r"
+expect "Disallow root login remotely? \[Y/n\]"
+send "Y\r"
+expect "Remove test database and access to it? \[Y/n\]"
+send "Y\r"
+expect "Reload privilege tables now? \[Y/n\]"
+send "Y\r"
+expect eof
+EOF
+
+mysql -u root -p${MYSQL_ROOT_PASSWORD} "
 CREATE DATABASE wordpress;
 CREATE USER 'admin'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}' WITH GRANT OPTION;
--- bonus/php.sh
+++ bonus/php.sh
@@ -2,3 +2,4 @@
 systemctl enable php8.2-fpm
 sed -i 's/^;cgi.fix_pathinfo=1/cgi.fix_pathinfo=1/' /etc/php/8.2/fpm/php.ini
 sed -i 's/^listen = \/run\/php\/php8.2-fpm.sock/listen = 127.0.0.1:80/' /etc/php/8.2/fpm/pool.d/www.conf
+systemctl restart php8.2-fpm
