3df95dbd057b8fdb33930053031fd1def52c7fda
Justin Lee <triplez@justinlee.sg>
2024-06-02 19:12:33 +0800

    Refactor lighttpd.sh to enable TCP traffic on port 80 and update PHP configuration

--- bonus/lighttpd.sh
+++ bonus/lighttpd.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 apt purge -y apache2
 systemctl enable lighttpd
-sed -i '/^### RULES ###/a\\n### tuple ### allow tcp 80 0.0.0.0/0 any 0.0.0.0/0 in\n-A ufw-user-input -p tcp --dport 80 -j ACCEPT\n\n### END RULES ###\n/' /etc/ufw/user.rules
-sed -i '/^### RULES ###/a\\n### tuple ### allow tcp 80 ::/0 any ::/0 in\n-A ufw6-user-input -p tcp --dport 80 -j ACCEPT\n\n### END RULES ###\n/' /etc/ufw/user6.rules
+sed -i '/^### RULES ###/a \
+### tuple ### allow tcp 80 0.0.0.0/0 any 0.0.0.0/0 in\n-A ufw-user-input -p tcp --dport 80 -j ACCEPT\n' /etc/ufw/user.rules
+sed -i '/^### RULES ###/a \
+### tuple ### allow tcp 80 ::/0 any ::/0 in\n-A ufw6-user-input -p tcp --dport 80 -j ACCEPT\n' /etc/ufw/user6.rules
 sed -i 's#"bin-path" => "\/usr\/bin\/php-cgi",/"host" => "127.0.0.1",#' /etc/lighttpd/conf-available/15-fastcgi-php.conf
 sed -i 's#"socket" => "\/run\/lighttpd\/php.socket",/"port" => "80",#' /etc/lighttpd/conf-available/15-fastcgi-php.conf
 lighty-enable-mod fastcgi
