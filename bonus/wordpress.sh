417b6d6d78a13665cc95a43fd9443d9cbe373e2c
Justin Lee <triplez@justinlee.sg>
2024-06-02 14:51:04 +0800

    Refactor wordpress.sh to use absolute path for downloading and extracting latest.tar.gz

--- bonus/wordpress.sh
+++ bonus/wordpress.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -O /tmp/latest.tar.gz http://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz -C /var/www/html/
+tar -xzvf /tmp/latest.tar.gz -C /var/www/html/
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
