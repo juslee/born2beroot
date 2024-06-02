7d273790491e64e3852cf3f4ec4dabda4d8a6c16
Justin Lee <triplez@justinlee.sg>
2024-06-02 23:37:45 +0800

    Refactor mariadb.sh to update root password encryption and remove unused expect package

--- bonus/mariadb.sh
+++ bonus/mariadb.sh
@@ -7,29 +7,14 @@ apt install -y expect
 MYSQL_ROOT_PASSWORD="mypassword"
 MYSQL_USER_PASSWORD="mypassword"
 
-expect -f - <<-EOF
-set timeout 2
-spawn mysql_secure_installation
-expect "Enter current password for root (enter for none):"
-send "\r"
-expect "Switch to unix_socket authentication \[Y/n\]"
-send "n\r"
-expect "Change the root password? \[Y/n\]"
-send "Y\r"
-expect "New password:"
-send "${MYSQL_ROOT_PASSWORD}\r"
-expect "Re-enter new password:"
-send "${MYSQL_ROOT_PASSWORD}\r"
-expect "Remove anonymous users?  \[Y/n\]"
-send "Y\r"
-expect "Disallow root login remotely? \[Y/n\]"
-send "Y\r"
-expect "Remove test database and access to it? \[Y/n\]"
-send "Y\r"
-expect "Reload privilege tables now? \[Y/n\]"
-send "Y\r"
-expect eof
-EOF
+mysql -e "
+UPDATE mysql.global_priv SET priv=json_set(priv, '$.plugin', 'mysql_native_password', '$.authentication_string', PASSWORD('$MYSQL_ROOT_PASSWORD')) WHERE User='root';
+DELETE FROM mysql.global_priv WHERE User='';
+DELETE FROM mysql.global_priv WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+"
 
 mysql -u root -p${MYSQL_ROOT_PASSWORD} "
 CREATE DATABASE wordpress_db;
