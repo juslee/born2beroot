e608907210d246b428d2ad8aea1a90484c9563bc
Justin Lee <triplez@justinlee.sg>
2024-06-03 12:21:49 +0800

    Refactor mariadb.sh to create WordPress database and user with proper privileges

--- bonus/mariadb.sh
+++ bonus/mariadb.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
+
+# Variables
+DB_NAME="wordpress_db"
+DB_USER="admin"
+DB_PASSWORD="mypassword"
+DB_ROOT_PASSWORD="mypassword"
+
+# Install MariaDB
+echo "mariadb-server mariadb-server/root_password password $DB_ROOT_PASSWORD" | debconf-set-selections
+echo "mariadb-server mariadb-server/root_password_again password $DB_ROOT_PASSWORD" | debconf-set-selections
+
+apt-get update
+apt-get install -y mariadb-server
+
 systemctl enable mariadb
 systemctl start mariadb
 
-MYSQL_ROOT_PASSWORD="mypassword"
-MYSQL_USER_PASSWORD="mypassword"
+# Secure MariaDB Installation
+mysql -e "UPDATE mysql.global_priv SET priv=json_set(priv, '$.plugin', 'mysql_native_password', '$.authentication_string', PASSWORD('$DB_ROOT_PASSWORD')) WHERE User='root';"
+mysql -e "DELETE FROM mysql.global_priv WHERE User='';"
+mysql -e "DELETE FROM mysql.global_priv WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+mysql -e "DROP DATABASE IF EXISTS test;"
+mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+mysql -e "FLUSH PRIVILEGES;"
 
-mysql -e "
-UPDATE mysql.global_priv SET priv=json_set(priv, '$.plugin', 'mysql_native_password', '$.authentication_string', PASSWORD('$MYSQL_ROOT_PASSWORD')) WHERE User='root';
-DELETE FROM mysql.global_priv WHERE User='';
-DELETE FROM mysql.global_priv WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEGES;
-"
-
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "
-CREATE DATABASE wordpress_db;
-CREATE USER 'admin'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
-GRANT ALL PRIVILEGES ON wordpress_db.* TO 'admin'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-"
+# Install MySQL Database and Create WordPress Database
+mysql -u root -p$DB_ROOT_PASSWORD -e "CREATE DATABASE $DB_NAME;"
+mysql -u root -p$DB_ROOT_PASSWORD -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -u root -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
+mysql -u root -p$DB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
--- bonus/wordpress.sh
+++ bonus/wordpress.sh
@@ -1,11 +1,54 @@
 #!/bin/bash
-wget -O /tmp/latest.tar.gz http://wordpress.org/latest.tar.gz
-tar -xzvf /tmp/latest.tar.gz -C /tmp
-mv /tmp/wordpress/* /var/www/html
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-sed -i 's/database_name_here/wordpress_db/' /var/www/html/wp-config.php
-sed -i 's/username_here/admin/' /var/www/html/wp-config.php
-sed -i 's/password_here/mypassword/' /var/www/html/wp-config.php
-sed -i 's/localhost/localhost/' /var/www/html/wp-config.php
-chown -R www-data:www-data /var/www/html/
-chmod -R 755 /var/www/html/
+
+# Variables
+DB_NAME="wordpress_db"
+DB_USER="admin"
+DB_PASSWORD="mypassword"
+WP_URL="http://localhost"
+WP_TITLE="My WordPress Site"
+WP_ADMIN_USER="admin"
+WP_ADMIN_PASSWORD="mypassword"
+WP_ADMIN_EMAIL="admin@example.com"
+WP_PATH="/var/www/html"
+
+# Download WordPress
+wget https://wordpress.org/latest.tar.gz -P /tmp
+tar -xzf /tmp/latest.tar.gz -C /tmp
+mv /tmp/wordpress/* $WP_PATH
+
+# Set Permissions
+sudo chown -R www-data:www-data $WP_PATH
+sudo chmod -R 755 $WP_PATH
+
+# Create wp-config.php
+cp $WP_PATH/wp-config-sample.php $WP_PATH/wp-config.php
+sed -i "s/database_name_here/$DB_NAME/" $WP_PATH/wp-config.php
+sed -i "s/username_here/$DB_USER/" $WP_PATH/wp-config.php
+sed -i "s/password_here/$DB_PASSWORD/" $WP_PATH/wp-config.php
+
+# Generate Authentication Keys and Salts
+curl -s https://api.wordpress.org/secret-key/1.1/salt/ > /tmp/wp.keys
+sed -i '/AUTH_KEY/d' $WP_PATH/wp-config.php
+sed -i '/SECURE_AUTH_KEY/d' $WP_PATH/wp-config.php
+sed -i '/LOGGED_IN_KEY/d' $WP_PATH/wp-config.php
+sed -i '/NONCE_KEY/d' $WP_PATH/wp-config.php
+sed -i '/AUTH_SALT/d' $WP_PATH/wp-config.php
+sed -i '/SECURE_AUTH_SALT/d' $WP_PATH/wp-config.php
+sed -i '/LOGGED_IN_SALT/d' $WP_PATH/wp-config.php
+sed -i '/NONCE_SALT/d' $WP_PATH/wp-config.php
+cat /tmp/wp.keys >> $WP_PATH/wp-config.php
+
+# Install WP-CLI
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+# Install WordPress
+cd $WP_PATH
+sudo -u www-data wp core install --url=$WP_URL --title="$WP_TITLE" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+
+# Clean up
+rm /tmp/latest.tar.gz
+rm /tmp/wp.keys
+
+echo "WordPress installation completed successfully!"
