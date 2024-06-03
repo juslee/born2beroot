5d1a514f86c02525f92c556aea408ab9c1d01c16
Justin Lee <triplez@justinlee.sg>
2024-06-03 11:21:16 +0800

    Refactor cron.sh to support dynamic URL parameter

--- mandatory/cron.sh
+++ mandatory/cron.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
-wget -O /usr/local/bin/monitoring.sh http://c2r1s14.42singapore.sg:8000/mandatory/monitoring.sh
+
+# Check if the URL parameter is provided
+if [ -z "$1" ]; then
+	echo "Usage: $0 <URL>"
+	exit 1
+fi
+
+# Assign the first parameter to a variable
+URL="$1"
+
+# Use the URL parameter in your script
+echo "The provided URL is: $URL"
+
+# Example: Download a file from the provided URL
+wget -O /usr/local/bin/monitoring.sh "$URL"
+
+# Check if the download was successful
+if [ $? -ne 0 ]; then
+	echo "Failed to download the file from $URL"
+	exit 1
+fi
+
+# Make the downloaded script executable
 chmod +x /usr/local/bin/monitoring.sh
+
+# Execute the downloaded script
 echo '*/10 * * * * /usr/local/bin/monitoring.sh' | crontab -u welee -
